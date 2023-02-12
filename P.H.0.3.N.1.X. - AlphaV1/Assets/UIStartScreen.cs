1a721833a92469dff3ddcebd3fd76072bcf52535
DangerousDaniel <danielcox996@gmail.com>
2023-02-12 19:08:58 -0400

    Start Screen Created Basic Functionally
    Use can press any key
    Hello world - Level 1 Open

---
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/UIStartScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStartScreen : MonoBehaviour
+{
+    void Update()
+    {
+        //Refernce to the level loader 
+
+        //Enum Sences
+
+        //Reference to Level 1
+
+        if (Input.anyKeyDown)
+        {
+            //open level one
+            Debug.Log("Open Level 1");
+        }
+    }
+}
