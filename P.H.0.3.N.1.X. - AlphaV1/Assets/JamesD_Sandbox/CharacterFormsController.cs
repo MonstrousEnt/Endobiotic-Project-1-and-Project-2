69c47845b17b4f00659470c6a0652d9d7e80a54a
RavengerINC <jamesmdalziel@gmail.com>
2023-02-12 14:15:55 -0400

    Added basic form swapping
    Forms swap when CharacterFormController.KillForm() is called
    Temporary input controller added to character

--- P.H.0.3.N.1.X. - AlphaV1/Assets/JamesD_Sandbox/CharacterFormsController.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/JamesD_Sandbox/CharacterFormsController.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class CharacterFormsController : MonoBehaviour
 {
     /// <summary>
@@ -9,24 +9,34 @@ public class CharacterFormsController : MonoBehaviour
     /// </summary>
 
     public Form currForm { get; private set; }
+    public Sprite[] formSprites;
+
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         Init();
     }
 
     public void KillForm()
     {
+        int numForms = Enum.GetNames(typeof(Form)).Length;
+        int currFormNum = (int)currForm;
+        int nextFormInt = (currFormNum + 1) % numForms;        
 
+        ChangeForm(nextFormInt);
     }
 
     private void Init()
     {
-        currForm = Form.Manipulator;
+        ChangeForm(0);        
     }
 
-    private void ChangeForm(Form newForm)
+    private void ChangeForm(int newFormInt)
     {
-        currForm = newForm;
+        currForm = (Form)newFormInt;
+        spriteRenderer.sprite = formSprites[newFormInt];
+        Debug.Log("Form changed to: " + (Form)newFormInt);
     }
 }
