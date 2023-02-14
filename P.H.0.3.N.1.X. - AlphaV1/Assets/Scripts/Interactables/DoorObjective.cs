8a80c9e66707312f45ef064f9ca793d24ac1a13f
RavengerINC <jamesmdalziel@gmail.com>
2023-02-13 20:22:55 -0400

    Added script to disable door and added it to the door prefab

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/DoorObjective.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/DoorObjective.cs
@@ -1,23 +1,9 @@
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Collider2D))]
 public class DoorObjective : MonoBehaviour
 {
-    [SerializeField] private Sprite doorOpen;
-
-    private SpriteRenderer m_spriteRenderer;
-    private Collider2D m_collider;
-
-    private void Start()
+    public void DisableDoor()
     {
-        m_spriteRenderer = GetComponent<SpriteRenderer>();
-        m_collider = GetComponent<Collider2D>();
-    }
-
-    public void OpenDoor()
-    {
-        m_spriteRenderer.sprite = doorOpen;
-        m_collider.enabled = false;
+        gameObject.SetActive(false);
     }
 }
