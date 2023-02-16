80bb76ee19a52e8f3217e26059ceac2fe5f867e8
DangerousDaniel <danielcox996@gmail.com>
2023-02-16 01:52:37 -0400

    Audio Manager - Sound Data Class: Custom Editor code commented out for the time being.

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/ScriptableObjects/SoundData/SoundData.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/ScriptableObjects/SoundData/SoundData.cs
@@ -32,48 +32,48 @@ public class LevelDataCustomEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        //Load Variables
-        SoundData soundDataConetent = (SoundData)target; //Get Scriptable Object Inspector
+        ////Load Variables
+        //SoundData soundDataConetent = (SoundData)target; //Get Scriptable Object Inspector
 
-        //Update the serialized object in the inspector
-        serializedObject.Update();
+        ////Update the serialized object in the inspector
+        //serializedObject.Update();
 
-        //Script reference
-        GUI.enabled = false; //make this read only field
-        EditorGUILayout.ObjectField("Script", MonoScript.FromScriptableObject((SoundData)target), typeof(SoundData), false);
+        ////Script reference
+        //GUI.enabled = false; //make this read only field
+        //EditorGUILayout.ObjectField("Script", MonoScript.FromScriptableObject((SoundData)target), typeof(SoundData), false);
 
-        GUI.enabled = true;
+        //GUI.enabled = true;
 
-        //Make a space in the editor
-        EditorGUILayout.Space();
+        ////Make a space in the editor
+        //EditorGUILayout.Space();
 
-        //Create a tile section for Sound Data Variables
-        EditorGUILayout.LabelField("Sound Data", EditorStyles.boldLabel);
+        ////Create a tile section for Sound Data Variables
+        //EditorGUILayout.LabelField("Sound Data", EditorStyles.boldLabel);
 
-        //User Input 
-        soundDataConetent.clip = (AudioClip)EditorGUILayout.ObjectField("Cilp", soundDataConetent.clip, typeof(AudioClip), true); //Clip
-        soundDataConetent.soundName = EditorGUILayout.TextField("Name", soundDataConetent.soundName); //Name
-        soundDataConetent.volume = EditorGUILayout.FloatField("Volume", soundDataConetent.volume); //Volume
-        soundDataConetent.pitch = EditorGUILayout.FloatField("Pitch", soundDataConetent.pitch); //Pitch
-        soundDataConetent.loop = EditorGUILayout.Toggle("Loop", soundDataConetent.loop); //Loop
-        soundDataConetent.playOnAwake = EditorGUILayout.Toggle("Play On Awake", soundDataConetent.playOnAwake); //Play on Awake
+        ////User Input 
+        //soundDataConetent.clip = (AudioClip)EditorGUILayout.ObjectField("Cilp", soundDataConetent.clip, typeof(AudioClip), true); //Clip
+        //soundDataConetent.soundName = EditorGUILayout.TextField("Name", soundDataConetent.soundName); //Name
+        //soundDataConetent.volume = EditorGUILayout.FloatField("Volume", soundDataConetent.volume); //Volume
+        //soundDataConetent.pitch = EditorGUILayout.FloatField("Pitch", soundDataConetent.pitch); //Pitch
+        //soundDataConetent.loop = EditorGUILayout.Toggle("Loop", soundDataConetent.loop); //Loop
+        //soundDataConetent.playOnAwake = EditorGUILayout.Toggle("Play On Awake", soundDataConetent.playOnAwake); //Play on Awake
 
 
-        //Apply changes
-        serializedObject.ApplyModifiedProperties();
+        ////Apply changes
+        //serializedObject.ApplyModifiedProperties();
 
-        //Save data when using press save project from file dropdown menu in the menutool bar
-        EditorUtility.SetDirty(target);
+        ////Save data when using press save project from file dropdown menu in the menutool bar
+        //EditorUtility.SetDirty(target);
 
-        //Play Button
-        //if  (GUILayout.Button("Play"))
-        //{
-        //    soundDataConetent.SetupAudioSource();
-        //    soundDataConetent.PlayAudio();
-        //}
+        ////Play Button
+        ////if  (GUILayout.Button("Play"))
+        ////{
+        ////    soundDataConetent.SetupAudioSource();
+        ////    soundDataConetent.PlayAudio();
+        ////}
 
         //Draws default ui(testing only)
-        //base.OnInspectorGUI();
+        base.OnInspectorGUI();
     }
 }
 #endif
