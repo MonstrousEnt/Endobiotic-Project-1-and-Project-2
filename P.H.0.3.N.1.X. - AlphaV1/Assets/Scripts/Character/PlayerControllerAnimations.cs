245f3a00f059223957031d0ac061def51c177e71
BenTopple <bentopple@gmail.com>
2023-02-16 18:01:35 -0400

    added destroyer animations

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Character/PlayerControllerAnimations.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Character/PlayerControllerAnimations.cs
@@ -12,6 +12,19 @@ public class PlayerControllerAnimations : MonoBehaviour
     [SerializeField] private string LastMoveDir = "D";
     [SerializeField] private float turnThresholdMoveY = 0.71f;
 
+    //Animation States
+    [Header("Animation States")]
+    [SerializeField] public const string IDLE_DOWN = "Idle_Down";
+    [SerializeField] private const string IDLE_UP = "Idle_Up";
+    [SerializeField] private const string IDLE_LEFT = "Idle_Left";
+    [SerializeField] private const string IDLE_RIGHT = "Idle_Right";
+
+    [SerializeField] private const string WALK_DOWN = "Walk_Down";
+    [SerializeField] private const string WALK_UP = "Walk_Up";
+    [SerializeField] private const string WALK_LEFT = "Walk_Left";
+    [SerializeField] private const string WALK_RIGHT = "Walk_Right";
+
+
     #region Animation Methods
 
     public void MovementAnimation(Vector2 movement)
