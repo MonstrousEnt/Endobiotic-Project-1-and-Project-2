0041e5283445967ecc6d265a59c237ba202b0476
BenTopple <bentopple@gmail.com>
2023-02-17 11:11:50 -0400

    Revert "Merge branch 'Dev' into Ben-Code-Animations-to-player-forms"
    This reverts commit 9679887ad38e49449897bee5d7a131d40d29b913, reversing
    changes made to 603b5c51e7ef552a36a1d578fdf44cc0e8bef5b9.

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/ScriptableObjects/SoundData/SoundList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SoundList", menuName = "Scriptable Objects/Sound List")]
-public class SoundList : ScriptableObject
-{
-    public List<SoundData> soundDatas = new List<SoundData>();
-}
