89b806e022a04b7252653a3c23266c514d9fba2d
DangerousDaniel <danielcox996@gmail.com>
2023-02-18 18:25:44 -0400

    Start Screen Sound working

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/UI/UIStartScreen.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/UI/UIStartScreen.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class UIStartScreen : MonoBehaviour
 {
+
+    [SerializeField] private SoundData soundDataTitleScreen;
+
+    private void Start()
+    {
+        GameMangerRootMaster.instance.audioManager.SetAudioLoop(soundDataTitleScreen, true);
+        GameMangerRootMaster.instance.audioManager.PlayAudio(soundDataTitleScreen);
+    }
+
     void Update()
     {
         if (Input.anyKeyDown)
         {
+            GameMangerRootMaster.instance.audioManager.SetAudioLoop(soundDataTitleScreen, false);
+            GameMangerRootMaster.instance.audioManager.StopAudio(soundDataTitleScreen);
+
             //open level one
             GameMangerRootMaster.instance.levelManager.InvokeLoadNextLevelUnityEvent(LevelName.MainLevel);
 
