e5c5ca785f17349a0fe83afbf011e925d0412c6b
DangerousDaniel <danielcox996@gmail.com>
2023-02-18 12:06:47 -0400

    UI Game Credits Done

---
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/UI/UIGameCredits.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGameCredits : MonoBehaviour
+{
+    [SerializeField] private GameObject mainWindowGameObject;
+
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private float animtionTimer;
+
+    private void Awake()
+    {
+        GameMangerRootMaster.instance.uIEvents.displayGameCreditsUnityEvent.AddListener(displayGameCredits);
+    }
+
+    private void Start()
+    {
+        displayGameCredits();
+    }
+
+    private void OnDestroy()
+    {
+        GameMangerRootMaster.instance.uIEvents.displayGameCreditsUnityEvent.RemoveListener(displayGameCredits);
+    }
+
+    private void displayGameCredits()
+    {
+        StopCoroutine(gameCreditsAnimation());
+        StartCoroutine(gameCreditsAnimation());
+    }
+
+    private IEnumerator gameCreditsAnimation()
+    {
+        //Enable Screen
+        mainWindowGameObject.SetActive(true);
+
+        //start animation
+        animator.Play("GameCreditsScrolling");
+
+        yield return new WaitForSeconds(animtionTimer);
+
+        //close window
+        mainWindowGameObject.SetActive(false);
+    }
+}
