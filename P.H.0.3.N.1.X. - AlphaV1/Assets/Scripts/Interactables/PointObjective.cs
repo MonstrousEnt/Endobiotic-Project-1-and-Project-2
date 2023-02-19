ac24588798bc2273ae71d5240e20f070e1e30914
DangerousDaniel <danielcox996@gmail.com>
2023-02-19 01:12:55 -0400

    Point - Circuit

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/PointObjective.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/PointObjective.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PointObjective : MonoBehaviour
 {
+    [SerializeField] private PiontData piontData;
+    [SerializeField] private PointList pointList;
+    [SerializeField] private PuzzlePointsList currPuzzlePointsList;
+
     public void AddPoints()
     {
-
+        pointList.AddToTheCollectPointsList(piontData, currPuzzlePointsList);
     }    
 }
