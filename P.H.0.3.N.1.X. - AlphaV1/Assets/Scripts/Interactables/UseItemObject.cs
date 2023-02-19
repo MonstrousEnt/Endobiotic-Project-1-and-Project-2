4d3f89dccd729dea51306fe42b87c15d693e9871
DangerousDaniel <danielcox996@gmail.com>
2023-02-19 01:01:30 -0400

    Point - Changer Block

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/UseItemObject.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/UseItemObject.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class UseItemObject : MonoBehaviour
 {
+    [SerializeField] private PiontData piontData;
+    [SerializeField] private PointList pointList;
+    [SerializeField] private PuzzlePointsList currPuzzlePointsList;
+
     public void UseItem()
     {
+        pointList.AddToTheCollectPointsList(piontData, currPuzzlePointsList);
+
         GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterItemHolder>().UseItem();
     }
 }
