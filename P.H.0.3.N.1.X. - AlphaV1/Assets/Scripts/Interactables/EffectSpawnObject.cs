8fd8345d6279e5d00470f4d3cdb89e47eb5b7f79
DangerousDaniel <danielcox996@gmail.com>
2023-03-17 22:44:35 -0300

    Interactable Code Clean Up Done

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/EffectSpawnObject.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/EffectSpawnObject.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class EffectSpawnObject : MonoBehaviour
 {
+    #region Class Variables
+    [Header("Components")]
     [SerializeField] private GameObject m_objectPrefab;
     [SerializeField] private Vector3 m_locationOffset = new Vector3(0, 0, 0);
+    #endregion
 
+    #region Spawn Object Methods
     public void SpawnObject()
     {
         Instantiate(m_objectPrefab, transform.position, Quaternion.identity, this.transform);
     }
+    #endregion
 }
