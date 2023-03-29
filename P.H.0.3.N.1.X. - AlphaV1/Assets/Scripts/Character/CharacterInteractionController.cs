9bda0903047d13819f439706f94c03e215a96140
DangerousDaniel <danielcox996@gmail.com>
2023-03-29 19:46:39 -0300

    Private name convention done for Character Interaction Controller

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Character/CharacterInteractionController.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Character/CharacterInteractionController.cs
@@ -8,64 +8,66 @@ public class CharacterInteractionController : MonoBehaviour
 {
     #region Class Variables
     [Header("Prefabs")]
-    [SerializeField] private GameObject deathPrefab;
+    [SerializeField] private GameObject m_deathPrefab;
 
     [Header("Special Effects")]
-    [SerializeField] private ParticleSystem riseAgainParticles;
+    [SerializeField] private ParticleSystem m_riseAgainParticles;
 
     [Header("Sounds")]
-    [SerializeField] private UnityEvent soundEffectUnityEvent;
+    [SerializeField] private UnityEvent m_soundEffectUnityEvent;
 
-    [Header("InvulTimer")]
-    [SerializeField] private float invulTime = 1f;
-    private float invulTimer;
+    [Header("Invulnerable Timer")]
+    [SerializeField] private float m_invulnerableTimeSF = 1f;
 
     [Header("Tags Scripbale Object")]
-    [SerializeField] private TagDataScriptableObject tagDataEnemy;
-    [SerializeField] private TagDataScriptableObject tagDataInteractable;
+    [SerializeField] private TagDataScriptableObject m_tagDataEnemy;
+    [SerializeField] private TagDataScriptableObject m_tagDataInteractable;
 
     [Header("Boolean Flag Scriptable Object - Player Manager")]
     [SerializeField] private BooleanFlagGlobalVariableScriptableObject m_booleanFlagGlobalVariablePlayerCanMove;
 
     //Components
-    private CharacterFormsController characterFormsController;
-    private CharacterItemHolder characterItemHolder;
+    private CharacterFormsController m_characterFormsController;
+    private CharacterItemHolder m_characterItemHolder;
 
     //Current Intractable
-    private List<Interactable> currentlyInteractable;
+    private List<Interactable> m_currentlyInteractable;
+
+    //Invulnerable Timer
+    private float m_invulnerableTimer;
     #endregion
 
     #region Unity Methods
     private void Awake()
     {
-        characterFormsController = GetComponent<CharacterFormsController>();
-        characterItemHolder = GetComponent<CharacterItemHolder>();
+        m_characterFormsController = GetComponent<CharacterFormsController>();
+        m_characterItemHolder = GetComponent<CharacterItemHolder>();
     }
 
     private void Start()
     {
-        currentlyInteractable = new List<Interactable>();
+        m_currentlyInteractable = new List<Interactable>();
 
-        invulTimer = Time.time;
+        m_invulnerableTimer = Time.time;
     }
 
     private void OnCollisionEnter2D(Collision2D collision2D)
     {
-        if (invulTimer > Time.time) 
+        if (m_invulnerableTimer > Time.time) 
         {
             return;
         }
 
-        if (collision2D.collider.CompareTag(tagDataEnemy.tagName) && collision2D.collider.GetComponent<CharacterFormsController>().currForm == Form.Crab && characterFormsController.currForm == Form.Destroyer) 
+        if (collision2D.collider.CompareTag(m_tagDataEnemy.tagName) && collision2D.collider.GetComponent<CharacterFormsController>().currForm == Form.Crab && m_characterFormsController.currForm == Form.Destroyer) 
         {
             return;
         }
-        else if (collision2D.collider.CompareTag(tagDataEnemy.tagName) && collision2D.collider.GetComponent<CharacterFormsController>().currForm == Form.Crab) 
+        else if (collision2D.collider.CompareTag(m_tagDataEnemy.tagName) && collision2D.collider.GetComponent<CharacterFormsController>().currForm == Form.Crab) 
         {
             respawnCrab();
         }
         //if the player collides with the enemy and is not the form of the enemy
-        else if (collision2D.collider.CompareTag(tagDataEnemy.tagName) && characterFormsController.currForm != collision2D.collider.GetComponent<CharacterFormsController>().currForm)
+        else if (collision2D.collider.CompareTag(m_tagDataEnemy.tagName) && m_characterFormsController.currForm != collision2D.collider.GetComponent<CharacterFormsController>().currForm)
         {
             respawnCharacter(collision2D);
         }
@@ -73,7 +75,7 @@ public class CharacterInteractionController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        if (collider2D.CompareTag(tagDataInteractable.tagName) || collider2D.CompareTag(tagDataEnemy.tagName))
+        if (collider2D.CompareTag(m_tagDataInteractable.tagName) || collider2D.CompareTag(m_tagDataEnemy.tagName))
         {
             addIntractable(collider2D);
         }   
@@ -90,14 +92,14 @@ public class CharacterInteractionController : MonoBehaviour
     #region Character Interaction Methods
     public void Interact()
     {
-        if (currentlyInteractable.Count <= 0)
+        if (m_currentlyInteractable.Count <= 0)
         {
             return;
         }
 
-        foreach (Interactable interactable in currentlyInteractable)
+        foreach (Interactable interactable in m_currentlyInteractable)
         {
-            interactable.Interact(characterFormsController.currForm);
+            interactable.Interact(m_characterFormsController.currForm);
         }
     }
 
@@ -105,7 +107,7 @@ public class CharacterInteractionController : MonoBehaviour
     {
         if (collider2D.TryGetComponent(out Interactable interactable))
         {
-            currentlyInteractable.Add(interactable);
+            m_currentlyInteractable.Add(interactable);
         }
     }
 
@@ -113,13 +115,13 @@ public class CharacterInteractionController : MonoBehaviour
     {
         if (collider2D.TryGetComponent(out Interactable interactable))
         {
-            currentlyInteractable.Remove(interactable);
+            m_currentlyInteractable.Remove(interactable);
         }
     }
 
     private void respawnCrab()
     {
-        characterItemHolder.DropItem();
+        m_characterItemHolder.DropItem();
 
         // TODO set a respawn location or enable a variable for a designer
         Vector2 randomLocation = new Vector2(Random.Range(-4.0f, 4.0f), Random.Range(-2.0f, 2.0f));
@@ -129,22 +131,22 @@ public class CharacterInteractionController : MonoBehaviour
     private void respawnAsNewForm(Form newForm, Vector3 position)
     {
    
-        characterItemHolder.DropItem();
+        m_characterItemHolder.DropItem();
 
         m_booleanFlagGlobalVariablePlayerCanMove.DisableBooleanFlag();
 
-        GameObject deathInstance = Instantiate(deathPrefab, transform.position, Quaternion.identity);
+        GameObject deathInstance = Instantiate(m_deathPrefab, transform.position, Quaternion.identity);
 
-        deathInstance.GetComponent<CharacterFormsController>().ChangeForm(characterFormsController.currForm);  // These were firing before Start() on deathInstance. Weird. 
+        deathInstance.GetComponent<CharacterFormsController>().ChangeForm(m_characterFormsController.currForm);  // These were firing before Start() on deathInstance. Weird. 
         deathInstance.GetComponent<CharacterDeathController>().Die();
 
-        riseAgainParticles.Play();
+        m_riseAgainParticles.Play();
 
-        characterFormsController.ChangeForm(newForm);
+        m_characterFormsController.ChangeForm(newForm);
 
         transform.position = position;
 
-        soundEffectUnityEvent?.Invoke();
+        m_soundEffectUnityEvent?.Invoke();
 
         StartCoroutine(waitWhileDead(2));
     }
@@ -155,7 +157,7 @@ public class CharacterInteractionController : MonoBehaviour
 
         collision2D.collider.GetComponent<EnemyInteraction>().DestroyEnemy();
 
-        invulTimer = Time.time + invulTime;
+        m_invulnerableTimer = Time.time + m_invulnerableTimeSF;
     }
 
     private IEnumerator waitWhileDead(float duration)
