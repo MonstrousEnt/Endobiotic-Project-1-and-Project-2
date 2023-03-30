095ab07a2d53835eaa0d1db762050c6efdc01b02
DangerousDaniel <danielcox996@gmail.com>
2023-03-29 22:29:25 -0300

    Private name convention done for Effect Use Item

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/EffectUseItem.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/Interactables/EffectUseItem.cs
@@ -8,13 +8,13 @@ public class EffectUseItem : MonoBehaviour
 {
     #region Class Variables
     [Header("Tag Scriptabe Object")]
-    [SerializeField] private TagDataScriptableObject tagDataPlayer;
+    [SerializeField] private TagDataScriptableObject m_tagDataPlayer;
     #endregion
 
     #region Item Methods
     public void UseItem()
     {
-        GameObject.FindGameObjectWithTag(tagDataPlayer.tagName).GetComponent<CharacterItemHolder>().UseItem();
+        GameObject.FindGameObjectWithTag(m_tagDataPlayer.tagName).GetComponent<CharacterItemHolder>().UseItem();
     }
     #endregion
 }
