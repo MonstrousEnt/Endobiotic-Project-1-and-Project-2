fbd13eeceae1808a9d51913311165139cf361e3c
DangerousDaniel <danielcox996@gmail.com>
2023-03-29 21:51:20 -0300

    Private name convention done for Trap Object

--- P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/PhysicsInteractions/TrapObject.cs
+++ P.H.0.3.N.1.X. - AlphaV1/Assets/Scripts/PhysicsInteractions/TrapObject.cs
@@ -7,40 +7,44 @@ public class TrapObject : MonoBehaviour
 {
     #region Class Variables
     [Header("Intractable")]
-    [SerializeField] private InteractableOjbects objectType;
+    [SerializeField] private InteractableOjbects m_objectType;
 
     [Header("Sprite")]
-    [SerializeField] private Sprite hasActivatedSprite;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite m_hasActivatedSprite;
+
 
     [Header("Unity Events")]
     [SerializeField] private UnityEvent m_addPointUnityEvent;
 
+    //Sprite
+    private SpriteRenderer m_spriteRenderer;
+
     //Trap
-    private Collider2D trapCollider;
+    private Collider2D m_trapCollider;
+
 
     #endregion
 
     #region Unity Methods 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        trapCollider = GetComponent<Collider2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_trapCollider = GetComponent<Collider2D>();
     }
     #endregion
 
     #region Intractable Methods
     public void Interact()
     {
-        spriteRenderer.sprite = hasActivatedSprite;
-        trapCollider.enabled = false;
+        m_spriteRenderer.sprite = m_hasActivatedSprite;
+        m_trapCollider.enabled = false;
 
         m_addPointUnityEvent?.Invoke();
     }
 
     public InteractableOjbects GetObjectType()
     {
-        return objectType;
+        return m_objectType;
     }
 
     #endregion
